995f04232d6437cba118d7153d1eee3488a4e879
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-01-13 11:41:08 +0100

    Parse sacct json

--- sacct-analyze.py
+++ sacct-analyze.py
@@ -23,7 +23,7 @@
 import json
 import subprocess
 import tempfile
-
+import typing
 
 def get_sacct_data():
     with tempfile.TemporaryFile("wb+") as outfile:
@@ -31,3 +31,41 @@ def get_sacct_data():
         outfile.seek(0)
         data = json.load(outfile)
     return data
+
+
+class SlurmJob(typing.NamedTuple):
+    job_name: str
+    cpu: int
+    memory: int
+    time: int
+    elapsed_time: int
+    cpu_time: float
+    max_rss: float
+    @classmethod
+    def from_job_dict(cls, job_dict):
+        allocated_cpu = 0
+        allocated_memory = 0
+        max_rss = 0
+        for d in job_dict["tres"]["allocated"]:
+            tp = d["type"]
+            if tp == "cpu":
+                allocated_cpu = d["count"]
+            elif tp == "mem":
+                allocated_memory = d["count"]
+        for d in job_dict["steps"]:
+            if d["step"]["id"]["step_id"] == "0":
+                for used in d["tres"]["requested"]["max"]:
+                    type = used["type"]
+                    if type == "mem":
+                        max_rss = used["count"] / (1024 * 1024)
+
+        return cls(
+            job_name=job_dict["name"],
+            elapsed_time=job_dict["time"]["elapsed"],
+            cpu_time = (job_dict["time"]["total"]["seconds"] +
+                        job_dict["time"]["total"]["microseconds"] / 10 ** 6),
+            memory=allocated_memory,
+            cpu=allocated_cpu,
+            time = job_dict["time"]["limit"]["number"] * 60,
+            max_rss=max_rss,
+        )
